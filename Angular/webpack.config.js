80c5a13c6f90715cc3e68e058e43322359062db3
pawan.singh <pawan.singh@digitaslbi.com>
2017-12-28 20:00:29 +0530

    init commit

---
+++ Angular/webpack.config.js
@@ -0,0 +1,51 @@
+﻿
+
+var path = require("path");
+
+module.exports = {
+    context: path.resolve(__dirname, "./Scripts/src"),
+    resolve: {
+        extensions: ['.ts']
+    },
+    entry: {
+        main: './main'
+    },
+    output: {
+        publicPath: '/Scripts/dist',
+        path: path.resolve(__dirname, './Scripts/dist'),
+        filename: '[name].build.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    }
+};
+
+
+
+//"use strict";
+
+//module.exports = {
+//    entry: "./src/file.js",
+//    output: {
+//        filename: "./dist/bundle.js"
+//    },
+//    devServer: {
+//        contentBase: ".",
+//        host: "localhost",
+//        port: 9000
+//    },
+//    module: {
+//        loaders: [
+//            {
+//                test: /\.jsx?$/,
+//                loader: "babel-loader"
+//            }
+//        ]
+//    }
+//};
