bd69db539a0b6e2422995fd464b01d626f1fbd5c
pawan.singh <pwn.singh14@gmail.com>
2018-01-01 11:14:10 +0000

    init commit

--- Angular/Scripts/src/app.component.ts
+++ Angular/Scripts/src/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    templateUrl: './Scripts/src/app.component.html',
+    styleUrls: ['./Scripts/src/app.component.css']
 })
 export class AppComponent {
   title = 'app';
--- Angular/Scripts/src/main.ts
+++ Angular/Scripts/src/main.ts
@@ -3,6 +3,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from "./app.module";
 
+import "zone.js";
+
 platformBrowserDynamic().bootstrapModule(AppModule)
     .catch(err => console.log(err));
 
